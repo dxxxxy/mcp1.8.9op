651f721ecda797a8c422302384808426321781e3
DxxxxY <introsdream@gmail.com>
2022-01-04 16:32:17 -0500

    initial commit

---
+++ setup.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Setup ^> Decompiling
+call decompile.bat
+echo Setup ^> Fixing src
+powershell -Command "(gc src\minecraft\net\minecraft\world\gen\structure\StructureMineshaftPieces.java) -replace 'lvt_5_1_', 'k' | Out-File -encoding ASCII src\minecraft\net\minecraft\world\gen\structure\StructureMineshaftPieces.java"
+echo Setup ^> Recompiling
+call updatemd5.bat
+echo Setup ^> Removing mc src
+del /s /q src\minecraft\*
+rmdir /s /q src\minecraft\net
+echo Setup ^> Copying op src
+xcopy /e /i /y op\* src\minecraft
+echo Setup ^> Done, should be OK
+pause
